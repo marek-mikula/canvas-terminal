95e7dfe683911611b5edecdda1fe0e41fb8db425
Marek Mikula <marek.mikula01@gmail.com>
2021-09-26 21:29:28 +0200

    feat: typing indicator

--- canvas-console.js
+++ canvas-console.js
@@ -1,52 +1,87 @@
 const options = {
-    frequency: 50,
-    canvas: {
-        background: {
-            r: 0,
-            g: 0,
-            b: 0,
-        },
-        height: 400,
-        width: 700,
-        padding: 5,
-    },
+	frequency: 25,
+	canvas: {
+		background: {
+			r: 0,
+			g: 0,
+			b: 0,
+		},
+		height: 400,
+		width: 700,
+		padding: 5,
+	},
+	text: {
+		color: {
+			r: 255,
+			g: 255,
+			b: 255,
+		},
+		font: '15px Consolas'
+	}
 };
 
-const strokeStyleRGB = ({r, g, b}) => {
-    return `rgb(${r},${g},${b})`;
+const rgbToColor = ({r, g, b}) => {
+	return `rgb(${r},${g},${b})`;
 };
+
+const drawBackground = () => {
+	canvas.getCtx().beginPath();
+	canvas.getCtx().rect(0, 0, options.canvas.width, options.canvas.height);
+	canvas.getCtx().fillStyle = rgbToColor(options.canvas.background);
+	canvas.getCtx().fill();
+};
+
+const typeIndicator = {
+	frequency: 450,
+	counter: 0,
+	shown: false,
+	updateState() {
+		if (this.counter >= this.frequency) {
+			this.shown = !this.shown;
+			this.counter = 0;
+		} else {
+			this.counter += options.frequency;
+		}
+	},
+	draw() {
+		this.updateState();
+		if (!this.shown) {
+			return;
+		}
+		canvas.getCtx().fillStyle = rgbToColor(options.text.color);
+		canvas.getCtx().font = options.text.font;
+		canvas.getCtx().fillText("|", 10, 50);
+	}
+}
+
+const chores = [
+	() => drawBackground(),
+	() => typeIndicator.draw()
+];
 
 const updateFn = () => {
-    canvas.drawBackground();
+	chores.forEach(chore => chore());
 };
 
 class Canvas {
-    #canvas = null;
+	#canvas = null;
 	#ctx = null;
 
 	#interval = null;
 
 	constructor() {
-        this.#canvas = document.getElementById('canvas-console');
+		this.#canvas = document.getElementById('canvas-console');
 		this.#ctx = this.getCanvas().getContext('2d');
-        this.setCanvasWidth(options.canvas.width);
-        this.setCanvasHeight(options.canvas.height);
+		this.setCanvasWidth(options.canvas.width);
+		this.setCanvasHeight(options.canvas.height);
 	};
 
-    setCanvas(canvas) {
-		this.#canvas = canvas;
-	};
-
-    getCanvas() {
+	getCanvas() {
 		return this.#canvas;
 	};
 
 	getCtx() {
 		return this.#ctx;
-	};
-
-	setCtx(ctx) {
-		this.#ctx = ctx;
 	};
 
 	setCanvasHeight(height) {
@@ -73,13 +108,6 @@ class Canvas {
 		clearInterval(this.#interval);
 		this.#interval = null;
 	};
-
-    drawBackground() {
-        this.#ctx.beginPath();
-        this.#ctx.rect(0, 0, options.canvas.width, options.canvas.height);
-        this.#ctx.fillStyle = strokeStyleRGB(options.canvas.background);
-        this.#ctx.fill();
-    };
 
 	start() {
 		this.startInterval();
